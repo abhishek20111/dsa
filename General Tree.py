dff2b0163e52a870fed007027dba1a639f707b61
white <whitecoder1@gmail.com>
2021-12-12 14:25:12 +0530

    itr is First program of tree(general tree)

--- General Tree.py
+++ General Tree.py
@@ -110,67 +110,137 @@
 
 #  --------------------------------------------------------Exrecise---------------------
 
+# class TreeNode:
+#     def __init__(self,name, designation):
+#         self.name = name
+#         self.designation = designation
+#         self.children = []
+#         self.parent = None
+#
+#     def get_lenth(self):
+#         level = 0
+#         p = self.parent
+#
+#         while p:
+#             level +=1
+#             p = p.parent
+#         return level
+#
+#     def print_tree(self, propert_name):
+#         if propert_name == 'both':
+#             value = self.name + " (" + self.designation + ")"
+#         elif propert_name == 'name':
+#             value = self.name
+#         else:
+#             value = self.designation
+#
+#         space = ' ' * self.get_lenth() * 3
+#         prefix = space + "|--"if self.parent else""
+#         print(prefix + value)
+#         if self.children:
+#             for child in self.children:
+#                 child.print_tree(propert_name)
+#
+#     def add_child(self, child):
+#         child.parent = self
+#         self.children.append(child)
+#
+# def build_management_tree():
+#     #  CTO mangment
+#     infra_head = TreeNode("Vishua","Infrastructure Head")
+#     infra_head.add_child(TreeNode("Dhaval", "Cloud Manager"))
+#     infra_head.add_child(TreeNode("Abhijit", "App Manger"))
+#
+#     cto = TreeNode("Chinmey","CTO")
+#     cto.add_child(infra_head)
+#     cto.add_child(TreeNode("Amir", "Application Head"))
+#
+#     #HR hierarcy
+#     hr_head = TreeNode("gels", "HR Head")
+#
+#     hr_head.add_child(TreeNode("Peter", "Recuitment Manger"))
+#     hr_head.add_child(TreeNode("Waqas", "Policy Mangeger"))
+#
+#     ceo = TreeNode("Nilupl","CEO")
+#     ceo.add_child(cto)
+#     ceo.add_child(hr_head)
+#
+#     return ceo
+#
+# if __name__ == '__main__':
+#     root_node = build_management_tree()
+#     root_node.print_tree("name")
+#     print("\n\n")
+#     root_node.print_tree("designation")
+#     print("\n\n")
+#     root_node.print_tree("both")
+#---------------------------------------------------Practice----------------------------
+
 class TreeNode:
-    def __init__(self,name, designation):
-        self.name = name
-        self.designation = designation
+    def __init__(self, data):
+        self.data = data
         self.children = []
         self.parent = None
-
-    def get_lenth(self):
+    def get_level(self):
         level = 0
         p = self.parent
-
         while p:
-            level +=1
+            level += 1
             p = p.parent
+
         return level
-
-    def print_tree(self, propert_name):
-        if propert_name == 'both':
-            value = self.name + " (" + self.designation + ")"
-        elif propert_name == 'name':
-            value = self.name
-        else:
-            value = self.designation
-
-        space = ' ' * self.get_lenth() * 3
-        prefix = space + "|--"if self.parent else""
-        print(prefix + value)
+    def print_tree(self, level):
+        if self.get_level() > level:
+            return
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|--"if self.parent else""
+        print(prefix + self.data)
         if self.children:
             for child in self.children:
-                child.print_tree(propert_name)
+                child.print_tree(level)
 
     def add_child(self, child):
         child.parent = self
         self.children.append(child)
 
-def build_management_tree():
-    #  CTO mangment
-    infra_head = TreeNode("Vishua","Infrastructure Head")
-    infra_head.add_child(TreeNode("Dhaval", "Cloud Manager"))
-    infra_head.add_child(TreeNode("Abhijit", "App Manger"))
+def build_location_tree():
+    root = TreeNode("Global")
 
-    cto = TreeNode("Chinmey","CTO")
-    cto.add_child(infra_head)
-    cto.add_child(TreeNode("Amir", "Application Head"))
+    india = TreeNode("India")
 
-    #HR hierarcy
-    hr_head = TreeNode("gels", "HR Head")
+    gujarat = TreeNode("Gujarat")
+    gujarat.add_child(TreeNode("Ahmedabad"))
+    gujarat.add_child(TreeNode("Baroda"))
 
-    hr_head.add_child(TreeNode("Peter", "Recuitment Manger"))
-    hr_head.add_child(TreeNode("Waqas", "Policy Mangeger"))
+    karnataka = TreeNode("Karnataka")
+    karnataka.add_child(TreeNode("Bangluru"))
+    karnataka.add_child(TreeNode("Mysore"))
 
-    ceo = TreeNode("Nilupl","CEO")
-    ceo.add_child(cto)
-    ceo.add_child(hr_head)
+    india.add_child(gujarat)
+    india.add_child(karnataka)
 
-    return ceo
+    usa = TreeNode("USA")
 
+    nj = TreeNode("New Jersey")
+    nj.add_child(TreeNode("Princeton"))
+    nj.add_child(TreeNode("Trenton"))
+
+    california = TreeNode("California")
+    california.add_child(TreeNode("San Francisco"))
+    california.add_child(TreeNode("Mountain View"))
+    california.add_child(TreeNode("Palo Alto"))
+
+    usa.add_child(nj)
+    usa.add_child(california)
+
+    root.add_child(india)
+    root.add_child(usa)
+
+    return root
 if __name__ == '__main__':
-    root_node = build_management_tree()
-    root_node.print_tree("name")
-    print("\n\n")
-    root_node.print_tree("designation")
-    print("\n\n")
-    root_node.print_tree("both")
+    root_mode = build_location_tree()
+    root_mode.print_tree(1)
+    print("\n")
+    root_mode.print_tree(2)
+    print("\n")
+    root_mode.print_tree(3)
